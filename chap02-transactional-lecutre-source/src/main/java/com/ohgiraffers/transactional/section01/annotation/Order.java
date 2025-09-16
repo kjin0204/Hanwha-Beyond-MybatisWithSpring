14aa94ed7069428f3914c73d1ce24f0be98495c5
HyeonggyuKang <kin0204@naver.com>
2025-09-16 12:07:45 +0900

    insert 이후 select Key를 사용 하여 마지막 insert key값 가져오기

--- chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/Order.java
+++ chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/Order.java
@@ -3,6 +3,10 @@ package com.ohgiraffers.transactional.section01.annotation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 @AllArgsConstructor
 public class Order {
@@ -10,4 +14,11 @@ public class Order {
     private String orderDate;
     private String orderTime;
     private int totalOrderPrice;
+
+    /* 설명. OrderDTO에 있던 값을 Order Entity로 옮기기 위한 생성자 */
+    public Order(LocalDate orderDate, LocalTime orderTime, int totalOrderPrice) {
+        this.orderDate = orderDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        this.orderTime = orderTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        this.totalOrderPrice = totalOrderPrice;
+    }
 }
--- chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/OrderService.java
+++ chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/OrderService.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -21,24 +22,51 @@ public class OrderService {
     }
 
     @Transactional
-    public void regisNewOrder(OrderDTO orderInfo){
+    public void regisNewOrder(OrderDTO orderInfo) {
         /* 설명. 1. 주문한 메뉴들의 코드만 추출(DTO에서) */
-        List<Integer> menuCodes = new ArrayList<>();
-        List<OrderMenuDTO> orderMenus = orderInfo.getOrderMenus();
-        for(OrderMenuDTO orderMenu : orderMenus){
-            menuCodes.add(orderMenu.getMenuCode());
-        }
+//        List<Integer> menuCodes = new ArrayList<>();
+//        List<OrderMenuDTO> orderMenus = orderInfo.getOrderMenus();
+//        for(OrderMenuDTO orderMenu : orderMenus){
+//            menuCodes.add(orderMenu.getMenuCode());
+//        }
+
+        List<Integer> menuCodes = orderInfo.getOrderMenus()
+                .stream()
+//                .map(x -> x.getMenuCode())
+                .map(OrderMenuDTO::getMenuCode)
+                .collect(Collectors.toList());
         Map<String, List<Integer>> map = new HashMap<>();
         map.put("menuCodes", menuCodes);
 
         /* 설명. 2. 주문한 메뉴별로 Menu엔티티에 담아서 조회(select)해 오기(부가적인 메뉴의 정보 추출(단가)) */
         List<Menu> menus = menuMapper.selectMenusByMenuCodes(map);
-        menus.forEach(System.out::println);
+//        menus.forEach(System.out::println);
 
         /* 설명. 3. 이 주문건에 대한 총 합계를 계산 */
+        int totalOrderPrice = calcTotalPrice(orderInfo.getOrderMenus(), menus);
 
         /* 설명. 4. 1부터 3까지 해서 tbl_order 테이블에 추가(insert) */
+        /* 설명. 4-1 OrderDTO -> Order */
+        Order order = new Order(orderInfo.getOrderDate(), orderInfo.getOrderTime()
+                , totalOrderPrice);
+
+        /* 설명. 4-2 Order로 tbl_order 테이블에 insert */
+        orderMapper.insertOrder(order); //select Key를 활용해서 insert 이후 seelct을 통해 한번의 DB와의 I/O를 발생시킴
+        System.out.println("주문 성공 후 방금 주문한 번호가 Order Entity에 들어있는지 확인: " + order.getOrderCode());
 
         /* 설명. 5. tbl_order_menu 테이블에도 주문한 메뉴 갯수만큼 메뉴를 추가(insert) */
+    }
+
+    /* 설명. 사용자가 주문한 각 메뉴와 수량에 따른 총 주문금액 계산용 메소드 */
+    private int calcTotalPrice(List<OrderMenuDTO> orderMenus, List<Menu> menus) {
+        int totalPrice = 0;
+        int orderMenuSize = orderMenus.size();          // 사용자가 주문한 메뉴의 종류 갯수
+        for (int i = 0; i < orderMenuSize; i++) {       // 사용자가 주문한 메뉴의 종류만큼 반복
+            OrderMenuDTO orderMenu = orderMenus.get(i); // 메뉴 수량 추출을 위해
+            Menu menu = menus.get(i);                   // 메뉴 단가 추출을 위해
+            totalPrice += orderMenu.getOrderAmount() * menu.getMenuPrice();
+        }
+
+        return totalPrice;
     }
 }
