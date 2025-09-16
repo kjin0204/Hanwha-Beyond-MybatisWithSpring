dc48cdedbb151980ca1e9897dfe04976d39590b0
HyeonggyuKang <kin0204@naver.com>
2025-09-16 14:05:13 +0900

    tbl_order_menu 리스트로 한번에 넣어보는 거 추가 해 봄

--- chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/OrderMapper.java
+++ chap02-transactional-lecutre-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/OrderMapper.java
@@ -2,7 +2,13 @@ package com.ohgiraffers.transactional.section01.annotation;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insertOrder(Order order);
+
+    void registOrderMenu(OrderMenu orderMenu);
+
+    void registOrderMenuList(List<OrderMenu> orderMenus);
 }
